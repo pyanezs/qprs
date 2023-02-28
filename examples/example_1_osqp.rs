4cb364901ac4f81a894c9848d78633992d852b81
Pablo Yanez <27428038+pyanezs@users.noreply.github.com>
2023-02-28 22:37:27 +0100

    Make variables snake case

--- examples/example_1_osqp.rs
+++ examples/example_1_osqp.rs
@@ -1,23 +1,22 @@
 use osqp::{CscMatrix, Problem, Settings};
 
 fn main() {
-    use osqp::{CscMatrix, Problem, Settings};
-
     // Define problem data
-    let P = &[[4.0, 1.0], [1.0, 2.0]];
-    let q = &[1.0, 1.0];
-    let A = &[[1.0, 1.0], [1.0, 0.0], [0.0, 1.0]];
-    let l = &[1.0, 0.0, 0.0];
-    let u = &[1.0, 0.7, 0.7];
+    let p_matrix = &[[4.0, 1.0], [1.0, 2.0]];
+    let q_vector = &[1.0, 1.0];
+    let a_matrix = &[[1.0, 1.0], [1.0, 0.0], [0.0, 1.0]];
+    let l_vector = &[1.0, 0.0, 0.0];
+    let u_vector = &[1.0, 0.7, 0.7];
 
     // Extract the upper triangular elements of `P`
-    let P = CscMatrix::from(P).into_upper_tri();
+    let p_matrix = CscMatrix::from(p_matrix).into_upper_tri();
 
     // Disable verbose output
     let settings = Settings::default().verbose(false);
 
     // Create an OSQP problem
-    let mut prob = Problem::new(P, q, A, l, u, &settings).expect("failed to setup problem");
+    let mut prob = Problem::new(p_matrix, q_vector, a_matrix, l_vector, u_vector, &settings)
+        .expect("failed to setup problem");
 
     // Solve problem
     let result = prob.solve();
